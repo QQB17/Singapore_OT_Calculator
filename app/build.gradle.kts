81be32c23a3d28e0d1c87581c016d2d8e428a582
QQB17 <baoqq8@gmail.com>
2025-04-12 20:59:04 +0800

    Add compose libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.services)
-
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -59,6 +59,18 @@ android {
             isDebuggable = true
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeCompiler {
+        reportsDestination = layout.buildDirectory.dir("compose_compiler")
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -87,24 +99,17 @@ dependencies {
     androidTestImplementation(libs.android.support.test.rules)
     androidTestImplementation(libs.android.support.test.runner)
 
+    val composeBom = platform(libs.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Compose dependencies
+    implementation(libs.bundles.compose.all)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+
+
     // Import Firebase BOM
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
-
-    /*implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.annotation:annotation:1.8.2")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:1.15.0")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("androidx.test:core:1.6.1")
-    testImplementation("org.mockito:mockito-core:1.10.19")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("com.android.support.test:rules:1.0.2")
-    androidTestImplementation("com.android.support.test:runner:1.0.2")
-
-    // Import Firebase BOM
-    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
-    implementation("com.google.firebase:firebase-analytics")*/
 }
